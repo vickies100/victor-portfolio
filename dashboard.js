24a0122c44ed157156011b4bf10cafcfca5a9411
Victor Onyango <onyangovokoth@gmail.com>
2026-06-21 16:10:23 +0300

    Improved authentication UI and dashboard

--- dashboard.js
+++ dashboard.js
@@ -8,36 +8,82 @@ if (!token) {
 }
 
 
+
 fetch("https://auth-system-52jo.onrender.com/api/auth/profile",
-    {
+{
 
-        method: "GET",
+    method:"GET",
 
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-
+    headers:{
+        Authorization:`Bearer ${token}`
     }
-)
+
+})
 
 .then(res => res.json())
+
 
 .then(data => {
 
 
     document.getElementById("message").innerHTML = `
 
-        <h2>Welcome ${data.user.name}</h2>
 
-        <p>Email: ${data.user.email}</p>
+        <div class="profile-box">
+
+
+            <h2>
+                Welcome 👋
+            </h2>
+
+
+            <h3>
+                ${data.user.name}
+            </h3>
+
+
+            <p>
+                Email:
+                ${data.user.email}
+            </p>
+
+
+            <p>
+                Account Status:
+                <strong>
+                    Active
+                </strong>
+            </p>
+
+
+        </div>
+
 
     `;
 
 
 })
 
+
 .catch(error => {
 
     console.log(error);
+
+});
+
+
+
+
+
+document
+.getElementById("logoutBtn")
+.addEventListener("click",()=>{
+
+
+    localStorage.removeItem("token");
+
+
+    window.location.href="login.html";
+
 
 });
